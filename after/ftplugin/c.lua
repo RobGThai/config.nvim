776272c32c49902e25ca6716dbdf103d0ab42118
Poohdish Rattanavijai <thisisrobg@gmail.com>
2024-12-10 15:46:25 +0700

    Add support to code folding

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -2,3 +2,7 @@ vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 vim.bo.expandtab = true
 vim.wo.colorcolumn = '+0'
+
+-- Code folding support
+-- Need to study this more here https://neovim.io/doc/user/fold.html#fold-syntax
+vim.opt.foldmethod = 'indent'
